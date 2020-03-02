fbb359930eee3781ffa5f72121fdcd048cae4971
Kyle Riner <rinerkyle97@gmail.com>
2020-03-02 02:50:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -38,6 +38,7 @@ def reduce_to_total(source_array, starting_point)
     total = starting_point
   else if starting_point == nil
     total = 0
+  else
   end  
   while i < source_array.length
     total = total + source_array[i]
