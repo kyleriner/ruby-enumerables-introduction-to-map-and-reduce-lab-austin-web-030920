f3dd490395374ed86e8d31cf363a64e690411660
Kyle Riner <rinerkyle97@gmail.com>
2020-03-02 03:04:16 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -61,9 +61,9 @@ end
 def reduce_to_any_true(source_array)
   i = 0
   while i < source_array.length do
-    if source_array[i] == false
-      return false
-    else return true
+    if source_array[i] == true
+      return true
+    else return false
     end
   end
 end
