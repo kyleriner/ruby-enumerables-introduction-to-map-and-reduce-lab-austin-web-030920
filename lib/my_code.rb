d7d3c8aee51b2eaa3e9bdb5b39e2a03bb9d861b4
Kyle Riner <rinerkyle97@gmail.com>
2020-03-02 02:30:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,7 +4,7 @@ def map_to_negativize(source_array)
   negative_array = []
   while i < source_array.length do
     negative_array[i] = source_array[i] * -1
-    i += 0
+    i += 1
   end
   return negative_array
 end
