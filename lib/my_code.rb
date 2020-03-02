8834f682c98ed06e0d6b4a685686a4fab169a6db
Kyle Riner <rinerkyle97@gmail.com>
2020-03-02 02:43:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -33,5 +33,12 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point)
-  
+  i = 0
+  total = 0
+  while i < source_array.length
+    total = total + source_array[i]
+    i += 1
+  end
+  return total
+end
 
