04514b028df21441db345a1e4cee9491fc264354
Kyle Riner <rinerkyle97@gmail.com>
2020-03-02 02:55:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -32,11 +32,11 @@ def map_to_square(source_array)
   return squared_array
 end
 
-def reduce_to_total(source_array, starting_point*)
+def reduce_to_total(optional_parameters*)
   i = 0
-  if starting_point
-    total = starting_point
-  else starting_point == nil
+  if optional_parameters[1]
+    total = optional_parameters[1]
+  else
     total = 0
   end
     
